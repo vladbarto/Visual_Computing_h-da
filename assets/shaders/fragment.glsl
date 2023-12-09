1ffb00523057675f5f12ab6b0c188c5a21d364dc
vladbarto <utclujvb@gmail.com>
2023-12-09 22:25:18 +0100

    Termin 4: modified shaders

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -6,12 +6,36 @@ in vec3 colorVS;
 out vec4 color;
 uniform float animation;
 
-void main(){
-    //color = vec4(colorVS.x, colorVS.y, colorVS.z, 1.0);
-    vec3 baseColor = vec3(colorVS.x, colorVS.y, colorVS.z);
-    vec3 animatedColor = baseColor * abs(sin(animation));
+in vec3 Normal, LightDir, ViewDir;
+uniform vec3 matDiffuse, lightColor;
+uniform vec3 lightColorAmbient;
+uniform vec3 matEmissive;
 
-    color = vec4(animatedColor, 1.0);
-    //color = vec4(colorVS.x*animation, colorVS.y-animation, colorVS.z-animation, 1.0);
+uniform vec3 matSpecular;
+uniform float matShininess;
+
+void main(){
+    // normalise everything necessary
+    vec3 N = normalize(Normal);
+    vec3 L = normalize(LightDir);
+
+    // diffuse component
+    float cosa = max(0.0, dot(N, L));
+    vec3 DiffuseTerm = matDiffuse * lightColor;
+    color = vec4(DiffuseTerm * cosa, 1.0);
+
+    // ambient emissive component
+    vec3 AmbientEmissiveTerm = matEmissive + matDiffuse * lightColorAmbient;
+    color += vec4(AmbientEmissiveTerm, 0.0);
+
+    // specular component
+    vec3 V = normalize(ViewDir);
+    vec3 R = normalize(reflect(-L, N));
+
+    float cosBeta = max(0.0, dot(R, V));
+    float cosBetak = pow(cosBeta, matShininess);
+
+    vec3 SpecularTerm = matSpecular * lightColor;
+    color += vec4(SpecularTerm * cosBetak, 0.0);
 }
 
